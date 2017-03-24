8b143b1d0d06334645de359770bc8b59991816dc
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2017-03-24 11:44:14 +0100

    Generate a big list

--- app/src/main/java/com/mcltechnologies/listview/MainActivity.java
+++ app/src/main/java/com/mcltechnologies/listview/MainActivity.java
@@ -1,12 +1,10 @@
 package com.mcltechnologies.listview;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<Element> elements = new ArrayList<>();
-        elements.add(new Element("Alice", "admin", Color.RED));
-        elements.add(new Element("Bob", "admin", Color.BLUE));
-        elements.add(new Element("Christophe", "admin", Color.GREEN));
+        for (int i = 0; i <= 2000; i++) {
+            elements.add(new Element("Alice", "admin", Color.RED));
+            elements.add(new Element("Bob", "admin", Color.BLUE));
+            elements.add(new Element("Christophe", "admin", Color.GREEN));
+        }
 
         ListView listView = (ListView)findViewById(R.id.list_view);
         ListAdapter listAdapter = new ElementAdapter(this, elements);
